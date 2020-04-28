3d787c78d70e7aaa07482f84138055a7ff562731
Brandon Jefferson <brandon@Brandons-MacBook-Air.local>
2020-04-28 11:04:07 -0400

    initial commit

---
+++ 1-jsFundamentals/1-grammarAndTypes/02-declarations.js
@@ -0,0 +1,48 @@
+let a = 2;
+
+......let......b......=........1
+/"....(1)......(2)....(3)....(4)
+
+1-keyword
+2-Variable name
+3-assignment operator
+4-Variable value
+
+"/
+
+console.log(a + b);
+
+/*
+-------notes on variables:
+-------a variable must begin with a SVGPathSegLinetoVerticalRel, underscore, or dollar 
+-----sign numbers may follow the above characters, but cannot come first
+-----javascript is case sensitive- hello and Hello are different variables
+-----camelCase is the best practice for naming variables
+------camelCase, snake-case, PascalCase  
+*/
+
+/*
+var c= 1;
+let d = 2;
+const e = 3; //value must remain constant
+
+//Declarations are the LEFT side of a variable
+-----it is simly: let x
+
+----Initialization are on the Right side of a variable
+---------it sets the value of the variable
+-----it incorporates the variable name, the assignment operator, and the value
+*/
+
+const x = 20; 
+console.log(x)
+
+x = 10;
+console.log(x)
+
+x = 33;
+console.log(x)
+
+let today = "First day"
+let mood = "Happy"
+console.log(today,mood)
